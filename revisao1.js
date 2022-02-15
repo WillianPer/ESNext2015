02b1f66bbc88ed0433daa1ea66972c0f4311742a
wilian Per <wsresilienttasteshopw@gmail.com>
2022-02-15 14:18:20 -0400

    Apresentando algumas nuanses trazidas pelas versoes ES6 ou superiores

---
+++ revisao1.js
@@ -0,0 +1,23 @@
+//var e let
+{
+    var a = 1
+    let b = 2
+
+    console.log(b)
+}
+console.log(a)
+
+//template string
+const produto = 'ipad'
+console.log(`${produto} é(?):
+caro!`)
+
+//destructuring
+const [l, e, ...tras] = 'cod3r'
+console.log(l, e, tras)
+
+const [x, , y] = [1, 2, 3]
+console.log(x, y)
+
+const {idade: i, nome} = {nome: 'Ana', idade: 9}
+console.log(i, nome)
