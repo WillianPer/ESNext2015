94cffaccbb91a96347cfaad0b24ed53ed83598c6
wilian Per <wsresilienttasteshopw@gmail.com>
2022-02-18 23:31:25 -0400

    Abordando o laço ForOF

---
+++ forOf.js
@@ -0,0 +1,41 @@
+for (let letra of 'cod3r') {
+    console.log(letra)
+}
+
+const assuntosEcma = ['set', 'map', 'promise']
+
+for (let i in assuntosEcma) {
+    console.log(i)
+}
+
+for (let assunto of assuntosEcma) {
+    console.log(assunto)
+}
+
+const assuntosMap = new Map([
+    ['map', { abordado : true }],
+    ['Set', { abordado : true }],
+    ['promise', { abordado : false}]
+])
+
+for (let assunto of assuntosMap) {
+    console.log(assunto)
+}
+
+for (let chaves of assuntosMap.keys()) {
+    console.log(chaves)
+}
+
+for (let valor of assuntosMap.values()) {
+    console.log(valor)
+}
+
+for (let [ch, vl] of assuntosMap.entries()) {
+    console.log(ch, vl)
+}
+
+const s = new Set(['a', 'b', 'c'])
+
+for (let letra of s) {
+    console.log(letra)
+}
